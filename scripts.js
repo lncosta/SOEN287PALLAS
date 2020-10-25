86f8ed146eb31a0ca2e2e363d2fff73f47c6e028
Luiza Costa <luiza.n16@gmail.com>
2020-10-25 10:28:57 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+  <!--SOEN 287 Group Project
+        Team 8 - PALLAS Entertainment
+        JavaScript
+        Team members:
+        Florian Charreau (26494889) 
+        Piyush Goyal(40106759) 
+        Aline Kurkdjian (40131528)
+        Joseph Mezzacappa(40134799)
+        Luiza Nogueira Costa (40124771)
+        Yi Heng Yan(40060587)
+    -->
