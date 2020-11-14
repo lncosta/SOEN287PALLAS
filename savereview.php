39ca471e3d00a661895b4cbb9d7de5f1585db178
Luiza Costa <luiza.n16@gmail.com>
2020-11-14 17:32:33 -0300

    Add files via upload

---
+++ savereview.php
@@ -0,0 +1,131 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "", "demo");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Escape user inputs for security
+ 
+$param_email =  mysqli_real_escape_string($link, $_REQUEST['email']);
+$param_fname =  mysqli_real_escape_string($link, $_REQUEST['fname']);
+$param_rating =  mysqli_real_escape_string($link, $_REQUEST['rating']);
+$param_userreview =  mysqli_real_escape_string($link, $_REQUEST['userreview']);
+
+// Attempt insert query execution
+$sql = "INSERT INTO reviews (email, fname, rating, userreview) VALUES ('$param_email', '$param_fname', '$param_rating', '$param_userreview')";
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
+// Close connection
+mysqli_close($link);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <!--Google Fonts-->
+    <!--Luckiest Guy || Montserrat-->
+    <link
+    href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&family=Montserrat:wght@400;700;900&display=swap"
+    rel="stylesheet"
+    />
+
+    
+    <style type="text/css">
+        body{
+            margin: 20px;
+        }
+        .mail{
+            margin: 20px;
+            padding: 20px;
+            text-align: left;
+            border: 2px solid blue;
+        }
+        .btn{
+            background-color: lightblue;
+            border: 5px solid lightblue;
+            border-radius: 5px;
+            color: black;
+            text-decoration: none;
+
+        }
+        .btn a{
+            color: black;
+
+        }
+       
+    </style>
+    <!--Manual CSS-->
+    <link rel="stylesheet" href="styles.css" />
+
+    <!--Icons-->
+    <script
+    src="https://kit.fontawesome.com/ba7a137c00.js"
+    crossorigin="anonymous"
+    ></script>
+
+     
+</head> 
+<body>
+    <section class="navbarsection">  
+        <!--NavBar-->
+            <div class="interiornav">
+                <ul class="navigation" id="navmenu">
+                    <li class="item">
+                        <a class="link" href="mainPage.html"><span class="navbar-brand">PALLAS</span></a>
+                    </li>
+                    <li class="item">
+                        <a class="link" href="ticketsales.php">Upcoming Performances</a>
+                    </li>
+                    <li class="item">
+                        <a class="link" href="contactform.php">Contact + Booking</a>
+                    </li>
+                    <li class="item">
+                        <a class="link" href="aboutpage.html">About</a>
+                    </li>
+                    <li class="item">
+                        <a class="link" href="performancearchive.html">Performance Gallery</a>
+                    </li>
+                    <li class="item">
+                        <a class="link" href="faq.html">FAQ</a>
+                    </li>
+                    <li class="item">
+                        <a class="link" href="userpage.php">User's Page</a>
+                    </li>
+                </ul>
+            </div>
+    </section>
+    <div>
+        <br/>
+        <h1>Hi, <b><?php echo $param_fname ?></b>. Your review has been added. Here is a copy of it:</h1>
+    </div>
+    <br/>
+    <div class="mail">
+        <h2>First Name: <b><?php echo $param_fname ?></b></h2>
+        <h2>Rating: <b><?php echo $param_rating ?></b>/5</h2>
+        <h2>Your Review: <b><?php echo $param_userreview ?></b></h2>
+    </div>  
+    <br/>
+    <p>
+        <a href="mainpage.html" class="btn">Return to Home Page</a>
+    </p>
+
+    <footer class="white-section" id="footer">
+        <div class="container-fluid">
+          <i class="footer-icon fab fa-twitter"></i>
+          <i class="footer-icon fab fa-facebook-f"></i>
+          <i class="footer-icon fab fa-instagram"></i>
+          <i class="footer-icon fas fa-envelope"></i>
+          <p>© Copyright 2020 PALLAS Entertainment</p>
+        </div>
+      </footer>
+</body>
+</html>
