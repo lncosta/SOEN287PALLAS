b68ff08cb66064d329e6de4793fdd54ebd76a70e
Luiza Costa <luiza.n16@gmail.com>
2020-11-24 17:04:24 -0500

    Improved code readability

--- login.php
+++ login.php
@@ -1,22 +1,20 @@
 <?php
-// Initialize the session
 session_start();
-// Check if the user is already logged in, if yes then redirect them to user page
+// Check if the user is already logged in and redirect to user page.
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: userpage.php");
     exit;
 }
-// Include config file
 require_once "config.php";
-// Define variables and initialize with empty values
+// Define parameter variables and intialize them as empty
 $email = $password = "";
 $email_err = $password_err = "";
-// Processing form data when form is submitted
+// Server-side validation based on mySQL protocol
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  
     // Check if email is empty
     if(empty(trim($_POST["email"]))){
-        $email_err = "Please enter email.";
+        $email_err = "Please enter an email.";
     } else{
         $email = trim($_POST["email"]);
     }
@@ -28,27 +26,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $password = trim($_POST["password"]);
     }
     
-    // Validate credentials
+    // Validate user input
     if(empty($email_err) && empty($password_err)){
-        // Prepare a select statement
         $sql = "SELECT id, email, password, fname, lname FROM users WHERE email = ?";
         
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_email);
-            // Set parameters
+        if($query = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($query, "s", $param_email); //Binds parameter to statement
             $param_email = $email;
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
-                // Check if email exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $email, $hashed_password, $fname, $lname);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
+            if(mysqli_stmt_execute($query)){ 
+                mysqli_stmt_store_result($query);
+                if(mysqli_stmt_num_rows($query) == 1){  //If the email is registered                
+                    mysqli_stmt_bind_result($query, $id, $email, $hashed_password, $fname, $lname); //Retrieves user information into variables
+                    if(mysqli_stmt_fetch($query)){
+                        if(password_verify($password, $hashed_password)){ //Verifies hashed password
                             session_start();
                             // Store data in session variables
                             $_SESSION["loggedin"] = true;
@@ -57,23 +47,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["fname"] = $fname; 
                             $_SESSION["lname"] = $lname; 
                             $_SESSION["discount"] = rand(5, 25);                                                                                
-                            // Redirect user to welcome page
+                            // Redirect user to user page
                             header("location: userpage.php");
                         } else{
-                            // Display an error message if password is not valid
-                            $password_err = "The password entered is not valid.";
+                            // Display an error message if password does not match
+                            $password_err = "The password does not match the account.";
                         }
                     }
                 } else{
-                    // Display an error message if email doesn't exist
-                    $email_err = "No account found for this email.";
+                    // Display an error message if the email is not registered
+                    $email_err = "This email has not been registered as an account. Please register before logging in.";
                 }
             } else{
                 echo "Something went wrong. Please try again later.";
             }
 
             // Close statement
-            mysqli_stmt_close($stmt);
+            mysqli_stmt_close($query);
             
         }
     }
@@ -88,7 +78,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-      <!--Google Fonts-->
+    <!--Google Fonts-->
     <!--Luckiest Guy || Montserrat-->
     <link
     href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&family=Montserrat:wght@400;700;900&display=swap"
@@ -102,19 +92,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         input{
             font-size: 1.05em;
             font-family: "Montserrat";
+            font-size: 1.05;
+            border: 2px solid grey;
+            border-radius: 5px;
+            width: 30%;
+            height: 1.5em;
         }
         .btn-login{
             background-color: lightblue;
             border: 5px solid lightblue;
             border-radius: 5px;
             font-size: 1.05em;
-        }
-        .input-box{
-            font-size: 1.05;
-            border: 2px solid grey;
-            border-radius: 5px;
-            width: 30%;
-            height: 1.5em;
+            width: 10%;
+            height: auto;
         }
     </style>
 
@@ -156,22 +146,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     </section>
     <div class="loginsection">
         <h2>Log in!</h2>
-        <p>Enter your information below to log in.</p>
+        <p>Enter your information below to log in to PALLAS.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                 <label>Email:</label>
                 <br/><br/>
-                <input type="text" class="input-box" name="email" value="<?php echo $email; ?>">
+                <input type="text" name="email" value="<?php echo $email; ?>"/>
                 <br/><br/>
-                <span class="help-block"><?php echo $email_err."<br/>"; ?></span>  
-                <label>Password</label>
+                <span><?php echo $email_err."<br/>"; ?></span>  
+                <label>Password:</label>
                 <br/><br/>
-                <input type="password" class="input-box" name="password"  value="<?php echo $password; ?>">
+                <input type="password" name="password"  value="<?php echo $password; ?>"/>
                 <br/><br/>
-                <span class="help-block"><?php echo $password_err."<br/>"; ?></span>
-                <input type="submit"  class="btn-login" value="Login">
-            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
-            <p>Forgot your password? <a href="">Reset Here</a>.</p>
+                <span><?php echo $password_err."<br/>"; ?></span>
+                <input type="submit"  class="btn-login" value="Login"/>
         </form>
+        <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        <p>Forgot your password? <a href="">Reset Here</a>.</p>
     </div>    
 
     <footer class="white-section" id="footer">
