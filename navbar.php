7d25250653aa91f4dbc2ed9dadc2ca60452a5255
Luiza Costa <luiza.n16@gmail.com>
2020-11-29 11:27:39 -0300

    Navbar template
    Include by adding:
     <?php include("navbar.php");?>

---
+++ navbar.php
@@ -0,0 +1,28 @@
+<section class="navbarsection">
+    <!--NavBar-->
+    <div class="interiornav">
+        <ul class="navigation" id="navmenu">
+            <li class="item">
+                <a class="link" href="mainPage.html"><span class="navbar-brand">PALLAS</span></a>
+            </li>
+            <li class="item">
+                <a class="link" href="ticketsales.php">Upcoming Performances</a>
+            </li>
+            <li class="item">
+                <a class="link" href="contactform.php">Contact + Booking</a>
+            </li>
+            <li class="item">
+                <a class="link" href="aboutpage.html">About</a>
+            </li>
+            <li class="item">
+                <a class="link" href="performancearchive.php">Performance Gallery</a>
+            </li>
+            <li class="item">
+                <a class="link" href="faq.html">FAQ</a>
+            </li>
+            <li class="item">
+                <a class="link" href="userpage.php">User's Page</a>
+            </li>
+        </ul>
+    </div>
+</section>
